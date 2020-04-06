afb688ccaa892bb2aab6f8b3ec8e32aefbd005af
catherrc <catherrc@usc.edu>
2020-04-05 22:53:48 -0700

    wokring menu overlay js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,37 @@
-$(document).ready(function() {
 
-    var t1 = gsap.timeline({ paused: true });
+var t1 = gsap.timeline({ paused: true });
 
-    t1.to(".one", {
-        y:6, 
-        duration: 1,
-        rotation: 45,
-        ease: back.inOut(1.7)
-    });
+t1.to(".one", {
+    y:6, 
+    backgroundColor: 000000,
+    duration: .5,
+    rotation: 45,
+    ease: "expo.inOut(1.7)"
+});
 
-    t1.to(".two", {
-        y: -6,
-        duration: 1,
-        rotation: -45,
-        ease: back.inOut(1.7),
-        delay: -0.8
-    });
+t1.to(".two", {
+    backgroundColor: 000000,
+    y: -6,
+    duration: .5,
+    rotation: -45,
+    ease: "expo.inOut(1.7)",
+    delay: -0.5
+});
 
-    t1.to("#nav-overlay", {
-        top: "0%",
-        duration: 1,
-        ease: expo.inOut(1.7),
-        delay: -2
-    });
+t1.to("#nav-overlay", {
+    top: "0%",
+    right: "0%",
+    duration: .8,
+    ease: "expo.inOut()",
+    delay: -.8
+});
 
-    t1.from("#nav-overlay ul li", {x: -200, duration: 1, opacity: 0, ease: back.inOut(1.7), stagger: .5});
+t1.from("#nav-overlay ul li", {x: -200, duration: .8, opacity: 0, ease: "back.inOut(1.7)", stagger: .2});
 
-    t1.reverse();
-    $(document).on("click", ".toggle-btn", function() {
-        t1.reversed(!t1.reversed());
-    });
-    $(document).on("click", "a", function() {
-        t1.reversed(!t1.reversed());
-    });
+t1.reverse();
+$(document).on("click", ".toggle", function() {
+    t1.reversed(!t1.reversed());
+});
+$(document).on("click", "a", function() {
+    t1.reversed(!t1.reversed());
 });
