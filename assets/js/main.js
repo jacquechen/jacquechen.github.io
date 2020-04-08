70bb379a67f3739ee38c3871923e6a572cfd5542
catherrc <catherrc@usc.edu>
2020-04-07 23:45:15 -0700

    front page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,40 @@
-
+var rule = CSSRulePlugin.getRule(".social ion-icon"); //get the rule
 var t1 = gsap.timeline({ paused: true });
+
 
 t1.to(".one", {
     y:6, 
-    backgroundColor: 000000,
-    duration: .5,
+    backgroundColor: "#ffffff",
+    duration: .8,
     rotation: 45,
-    ease: "expo.inOut(1.7)"
+    ease: "expo.inOut()"
 });
 
 t1.to(".two", {
-    backgroundColor: 000000,
+    backgroundColor: "#ffffff",
     y: -6,
-    duration: .5,
+    duration: .8,
     rotation: -45,
-    ease: "expo.inOut(1.7)",
-    delay: -0.5
+    ease: "expo.inOut()",
+    delay: -0.8
+});
+
+t1.to(".jackie", {
+    color: "#ffffff",
+    ease: "expo.inOut()",
+    delay: -.5
+});
+
+t1.to(".social ion-icon", {
+    color: "#ffffff",
+    ease: "expo.inOut()",
+    delay: -.5
+});
+
+t1.to(".contact a", {
+    color: "#ffffff",
+    ease: "expo.inOut()",
+    delay: -.5
 });
 
 t1.to("#nav-overlay", {
@@ -26,7 +45,7 @@ t1.to("#nav-overlay", {
     delay: -.8
 });
 
-t1.from("#nav-overlay ul li", {x: -200, duration: .8, opacity: 0, ease: "back.inOut(1.7)", stagger: .2});
+t1.from("#nav-overlay ul li", {x: -200, duration: .8, opacity: 0, ease: "back.inOut(1.7)", stagger: .1, delay: -0.5});
 
 t1.reverse();
 $(document).on("click", ".toggle", function() {
