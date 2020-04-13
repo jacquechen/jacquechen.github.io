4c641a663b7dbe43399f4a71adca7d9e9620c7fa
catherrc <catherrc@usc.edu>
2020-04-12 21:49:02 -0700

    rest of the site

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,45 @@ $(document).ready(function() {
 		$('[data-tabs]').css('min-height', height + 40 + 'px');
    
 }(jQuery, document));
+
+// Create a lightbox
+(function() {
+  var $lightbox = $("<div class='lightbox'></div>");
+  var $img = $("<img>");
+  var $caption = $("<p class='caption'></p>");
+
+  // Add image and caption to lightbox
+
+  $lightbox
+    .append($img)
+    .append($caption);
+
+  // Add lighbox to document
+
+  $('body').append($lightbox);
+
+  $('.lightbox-gallery img').click(function(e) {
+    e.preventDefault();
+
+    // Get image link and description
+    var src = $(this).attr("data-image-hd");
+    var cap = $(this).attr("alt");
+
+    // Add data to lighbox
+
+    $img.attr('src', src);
+    $caption.text(cap);
+
+    // Show lightbox
+
+    $lightbox.fadeIn('fast');
+
+    $lightbox.click(function() {
+      $lightbox.fadeOut('fast');
+    });
+  });
+
+}());
+
+
+
